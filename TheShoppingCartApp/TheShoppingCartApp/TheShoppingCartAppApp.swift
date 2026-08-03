d37611a79a8ad55ff673e3ad25fc1010757b1471
Dnyaneshwar Surywanshi <dnyaneshwar.surywanshi@fujitsu.com>
2026-08-03 22:28:16 +0530

    Project created First commit

---
+++ TheShoppingCartApp/TheShoppingCartApp/TheShoppingCartAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheShoppingCartAppApp.swift
+//  TheShoppingCartApp
+//
+//  Created by Dnyaneshwar Surywanshi on 03/08/26.
+//
+
+import SwiftUI
+
+@main
+struct TheShoppingCartAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TheShoppingCartApp/TheShoppingCartAppTests/TheShoppingCartAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  TheShoppingCartAppTests.swift
+//  TheShoppingCartAppTests
+//
+//  Created by Dnyaneshwar Surywanshi on 03/08/26.
+//
+
+import Testing
+@testable import TheShoppingCartApp
+
+struct TheShoppingCartAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
